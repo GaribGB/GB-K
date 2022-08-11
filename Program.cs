e33af7c1569b87ef7fa20ed280770f327202b353
Garib <garibabbasov@yandex.ru>
2022-08-12 00:28:03 +0700

    add Inpud

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
 ﻿
+Console.Clear();
+int quantity = Input("Введите количество элементов в исходном массиве: ");
+string[] arrayString = new string [quantity];
+
+PrintArray(arrayResult, quantityResult);
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
